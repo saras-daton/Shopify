7e920d43ab20b5d5db2df47d2f567dcc2be0fb7f
Abhishek <abhishek.baruah@sarasanalytics.com>
2023-03-30 09:39:36 +0530

    update 1.1.2

---
+++ models/Shopify/ShopifyOrdersDiscountApplications.sql
@@ -0,0 +1,163 @@
+{% if var('ShopifyOrdersDiscountApplications') %}
+{{ config( enabled = True ) }}
+{% else %}
+{{ config( enabled = False ) }}
+{% endif %}
+
+{% if var('currency_conversion_flag') %}
+-- depends_on: {{ ref('ExchangeRates') }}
+{% endif %}
+
+{% if is_incremental() %}
+{%- set max_loaded_query -%}
+SELECT coalesce(MAX(_daton_batch_runtime) - 2592000000,0) FROM {{ this }}
+{% endset %}
+
+{%- set max_loaded_results = run_query(max_loaded_query) -%}
+
+{%- if execute -%}
+{% set max_loaded = max_loaded_results.rows[0].values()[0] %}
+{% else %}
+{% set max_loaded = 0 %}
+{%- endif -%}
+{% endif %}
+
+{% set table_name_query %}
+{{set_table_name('%shopify%orders%')}}    
+{% endset %}  
+
+{% set results = run_query(table_name_query) %}
+{% if execute %}
+{# Return the first column #}
+{% set results_list = results.columns[0].values() %}
+{% set tables_lowercase_list = results.columns[1].values() %}
+{% else %}
+{% set results_list = [] %}
+{% set tables_lowercase_list = [] %}
+{% endif %}
+
+{% for i in results_list %}
+    {% if var('get_brandname_from_tablename_flag') %}
+        {% set brand =i.split('.')[2].split('_')[var('brandname_position_in_tablename')] %}
+    {% else %}
+        {% set brand = var('default_brandname') %}
+    {% endif %}
+
+    {% if var('get_storename_from_tablename_flag') %}
+        {% set store =i.split('.')[2].split('_')[var('storename_position_in_tablename')] %}
+    {% else %}
+        {% set store = var('default_storename') %}
+    {% endif %}
+
+    {% if var('timezone_conversion_flag') and i.lower() in tables_lowercase_list and i in var('raw_table_timezone_offset_hours')%}
+        {% set hr = var('raw_table_timezone_offset_hours')[i] %}
+    {% else %}
+        {% set hr = 0 %}
+    {% endif %}
+
+    SELECT *, ROW_NUMBER() OVER (PARTITION BY order_id order by _daton_batch_runtime desc) _seq_id
+    from (
+    SELECT * {{exclude()}} (row_num)
+    FROM (
+        select 
+        '{{brand}}' as brand,
+        '{{store}}' as store,
+        a.admin_graphql_api_id,
+        cast(a.id as string) order_id, 
+        email,
+        closed_at,
+        cast(a.created_at as {{ dbt.type_timestamp() }}) created_at,
+        CAST(a.updated_at as timestamp) updated_at,
+        number,
+        note,
+        token,
+        a.gateway,
+        a.test,
+        total_price,
+        subtotal_price,
+        total_weight,
+        total_tax,
+        taxes_included,
+        a.currency,
+        financial_status,
+        confirmed,
+        total_discounts,
+        total_line_items_price,
+        cart_token,
+        buyer_accepts_marketing,
+        name,
+        referring_site,
+        landing_site,
+        cancelled_at,
+        cancel_reason,
+        total_price_usd,
+        checkout_token,
+        reference,
+        user_id,
+        a.location_id,
+        source_identifier,
+        source_url,
+        CAST(a.processed_at as timestamp) processed_at,
+        a.device_id,
+        phone,
+        customer_locale,
+        app_id,
+        browser_ip,
+        landing_site_ref,
+        order_number,
+        payment_gateway_names,
+        processing_method,
+        checkout_id,
+        a.source_name,
+        fulfillment_status,
+        tags,
+        contact_email,
+        order_status_url,
+        {% if target.type =='snowflake' %}
+        discount_applications.VALUE:target_type::VARCHAR as discount_applications_target_type,
+        discount_applications.VALUE:type::VARCHAR as discount_applications_type,
+        discount_applications.VALUE:value::VARCHAR as discount_applications_value,
+        discount_applications.VALUE:value_type::VARCHAR as discount_applications_value_type,
+        discount_applications.VALUE:allocation_method::VARCHAR as discount_applications_allocation_method,
+        discount_applications.VALUE:target_selection::VARCHAR as discount_applications_target_selection,
+        discount_applications.VALUE:code::VARCHAR as discount_applications_code,
+        discount_applications.VALUE:title::VARCHAR as discount_applications_title,
+        discount_applications.VALUE:description::VARCHAR as discount_applications_description,
+        {% else %}
+        discount_applications.target_type as discount_applications_target_type,
+        discount_applications.type as discount_applications_type,
+        discount_applications.value as discount_applications_value,
+        discount_applications.value_type as discount_applications_value_type,
+        discount_applications.allocation_method as discount_applications_allocation_method,
+        discount_applications.target_selection as discount_applications_target_selection,
+        discount_applications.code as discount_applications_code,
+        discount_applications.title as discount_applications_title,
+        discount_applications.description as discount_applications_description,
+        {% endif %}
+        {% if var('currency_conversion_flag') %}
+            case when c.value is null then 1 else c.value end as exchange_currency_rate,
+            case when c.from_currency_code is null then a.currency else c.from_currency_code end as exchange_currency_code,
+        {% else %}
+            cast(1 as decimal) as exchange_currency_rate,
+            a.currency as exchange_currency_code,
+        {% endif %}
+        a.{{daton_user_id()}} as _daton_user_id,
+        a.{{daton_batch_runtime()}} as _daton_batch_runtime,
+        a.{{daton_batch_id()}} as _daton_batch_id,
+        current_timestamp() as _last_updated,
+        '{{env_var("DBT_CLOUD_RUN_ID", "manual")}}' as _run_id,
+        Dense_Rank() OVER (PARTITION BY a.id order by a.{{daton_batch_runtime()}} desc) row_num
+            from {{i}} a
+                {% if var('currency_conversion_flag') %}
+                    left join {{ref('ExchangeRates')}} c on date(a.created_at) = c.date and a.currency = c.to_currency_code
+                {% endif %}
+                {{unnesting("discount_applications")}}
+                {% if is_incremental() %}
+                {# /* -- this filter will only be applied on an incremental run */ #}
+                WHERE a.{{daton_batch_runtime()}}  >= {{max_loaded}}
+                {% endif %}
+
+        )
+        where row_num = 1)
+    {% if not loop.last %} union all {% endif %}
+{% endfor %}
