7abcfabca0dbf9cd2442be369a07547ff042e6a0
abijithmenon-saras <141811659+abijithmenon-saras@users.noreply.github.com>
2023-08-10 18:46:20 +0530

    Update ShopifyInventoryLevels.sql

--- models/Shopify/ShopifyInventoryLevels.sql
+++ models/Shopify/ShopifyInventoryLevels.sql
@@ -19,8 +19,7 @@ select coalesce(max(_daton_batch_runtime) - 2592000000,0) from {{ this }}
 {% endif %}
 
 {% set table_name_query %}
-{{set_table_name('%shopify%inventory_levels')}} and lower(table_name) not like '%googleanalytics%' and lower(table_name) not like 'v1%'
-{% endset %}  
+{{set_table_name('%shopify%inventory_levels')}}
 
 {% set results = run_query(table_name_query) %}
 {% if execute %}
@@ -63,13 +62,13 @@ select coalesce(max(_daton_batch_runtime) - 2592000000,0) from {{ this }}
     {{daton_batch_runtime()}} as _daton_batch_runtime,
     {{daton_batch_id()}} as _daton_batch_id,
     current_timestamp() as _last_updated,
-    '{{env_var("DBT_CLOUD_RUN_ID", "manual")}}' as _run_id,
+    '{{env_var("DBT_CLOUD_RUN_ID", "manual")}}' as _run_id
     from  {{i}} a
             {% if is_incremental() %}
             {# /* -- this filter will only be applied on an incremental run */ #}
             where a.{{daton_batch_runtime()}}  >= {{max_loaded}}
             {% endif %}  
-    qualify dense_rank() over (partition by inventory_item_id order by {{daton_batch_runtime()}}, {{daton_batch_id()}} desc) = 1
+    qualify dense_rank() over (partition byinventory_item_id order by {{daton_batch_runtime()}}, {{daton_batch_id()}} desc) = 1
 
     {% if not loop.last %} union all {% endif %}
 {% endfor %}
