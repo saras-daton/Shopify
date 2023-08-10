d692635269d7340baef0608966e91a0bfb772568
Jcp-Saras <joe.payappilly@sarasanalytics.com>
2023-08-10 17:24:10 +0530

    Update ShopifyPayouts.sql

--- models/Shopify/ShopifyPayouts.sql
+++ models/Shopify/ShopifyPayouts.sql
@@ -19,7 +19,7 @@
 {% endif %}
 
 {% set table_name_query %}
-    {{ set_table_name('%shopify%payouts') }} and lower(table_name) not like '%googleanalytics%' and lower(table_name) not like 'v1%'
+    {{ set_table_name('%shopify%payouts') }}
 {% endset %}
 
 {% set results = run_query(table_name_query) %}
@@ -59,29 +59,16 @@
         cast(date as date) as date,
         currency,
         cast(amount as numeric) as amount,
-        {% if target.type == 'snowflake' %}
-            cast(summary.value:adjustments_fee_amount as numeric) as summary_adjustments_fee_amount,
-            cast(summary.value:adjustments_gross_amount as numeric) as summary_adjustments_gross_amount,
-            cast(summary.value:charges_fee_amount as numeric) as summary_charges_fee_amount,
-            cast(summary.value:charges_gross_amount as numeric) as summary_charges_gross_amount,
-            cast(summary.value:refunds_fee_amount as numeric) as summary_refunds_fee_amount,
-            cast(summary.value:refunds_gross_amount as numeric) as summary_refunds_gross_amount,
-            cast(summary.value:reserved_funds_fee_amount as numeric) as summary_reserved_funds_fee_amount,
-            cast(summary.value:reserved_funds_gross_amount as numeric) as summary_reserved_funds_gross_amount,
-            cast(summary.value:retried_payouts_fee_amount as numeric) as summary_retried_payouts_fee_amount,
-            cast(summary.value:retried_payouts_gross_amount as numeric) as summary_retried_payouts_gross_amount,
-        {% else %}
-            cast(summary.adjustments_fee_amount as numeric) as summary_adjustments_fee_amount,
-            cast(summary.adjustments_gross_amount as numeric) as summary_adjustments_gross_amount,
-            cast(summary.charges_fee_amount as numeric) as summary_charges_fee_amount,
-            cast(summary.charges_gross_amount as numeric) as summary_charges_gross_amount,
-            cast(summary.refunds_fee_amount as numeric) as summary_refunds_fee_amount,
-            cast(summary.refunds_gross_amount as numeric) as summary_refunds_gross_amount,
-            cast(summary.reserved_funds_fee_amount as numeric) as summary_reserved_funds_fee_amount,
-            cast(summary.reserved_funds_gross_amount as numeric) as summary_reserved_funds_gross_amount,
-            cast(summary.retried_payouts_fee_amount as numeric) as summary_retried_payouts_fee_amount,
-            cast(summary.retried_payouts_gross_amount as numeric) as summary_retried_payouts_gross_amount,
-        {% endif %}
+        {{extract_nested_value("summary","adjustments_fee_amount","numeric")}} as summary_adjustments_fee_amount,
+        {{extract_nested_value("summary","adjustments_gross_amount","numeric")}} as summary_adjustments_gross_amount,
+        {{extract_nested_value("summary","charges_fee_amount","numeric")}} as summary_charges_fee_amount,
+        {{extract_nested_value("summary","charges_gross_amount","numeric")}} as summary_charges_gross_amount,
+        {{extract_nested_value("summary","refunds_fee_amount","numeric")}} as summary_refunds_fee_amount,
+        {{extract_nested_value("summary","refunds_gross_amount","numeric")}} as summary_refunds_gross_amount,
+        {{extract_nested_value("summary","reserved_funds_fee_amount","numeric")}} as summary_reserved_funds_fee_amount,
+        {{extract_nested_value("summary","reserved_funds_gross_amount","numeric")}} as summary_reserved_funds_gross_amount,
+        {{extract_nested_value("summary","retried_payouts_fee_amount","numeric")}} as summary_retried_payouts_fee_amount,
+        {{extract_nested_value("summary","retried_payouts_gross_amount","numeric")}} as summary_retried_payouts_gross_amount,
         {{ daton_user_id() }} as _daton_user_id,
         {{ daton_batch_runtime() }} as _daton_batch_runtime,
         {{ daton_batch_id() }} as _daton_batch_id,
@@ -94,6 +81,6 @@
         where {{ daton_batch_runtime() }} >= {{ max_loaded }}
     {% endif %}
 
-    qualify dense_rank() over (partition BY a.id order by {{ daton_batch_runtime() }} desc) = 1
+    qualify dense_rank() over (partition by a.id order by {{ daton_batch_runtime() }} desc) = 1
     {% if not loop.last %} union all {% endif %}
 {% endfor %}
