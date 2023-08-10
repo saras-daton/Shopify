888e52e4a537102408af3c3702aa448a342bee53
abijithmenon-saras <141811659+abijithmenon-saras@users.noreply.github.com>
2023-08-10 18:45:28 +0530

    Update ShopifyGiftCards.sql

--- models/Shopify/ShopifyGiftCards.sql
+++ models/Shopify/ShopifyGiftCards.sql
@@ -20,7 +20,7 @@ select coalesce(max(_daton_batch_runtime) - 2592000000,0) from {{ this }}
 
 
 {% set table_name_query %}
-{{set_table_name('%shopify%gift_cards')}} and lower(table_name) not like '%googleanalytics%' and lower(table_name) not like 'v1%'
+{{set_table_name('%shopify%gift_cards')}}
 {% endset %}  
 
 
@@ -51,7 +51,6 @@ select coalesce(max(_daton_batch_runtime) - 2592000000,0) from {{ this }}
         {% set hr = var('raw_table_timezone_offset_hours')[i] %}
     {% else %}
         {% set hr = 0 %}
-    {% endif %}
 
         select 
         '{{brand}}' as brand,
@@ -59,7 +58,7 @@ select coalesce(max(_daton_batch_runtime) - 2592000000,0) from {{ this }}
         cast(id as string) as id,
         balance,
         cast({{ dbt.dateadd(datepart="hour", interval=hr, from_date_or_timestamp="created_at") }} as {{ dbt.type_timestamp() }}) as created_at,
-        cast({{ dbt.dateadd(datepart="hour", interval=hr, from_date_or_timestamp="updated_at") }} as {{ dbt.type_timestamp() }}) as updated_at,
+        cast({{ dbt.dateadd(datepart="hour", interval=hr, from_date_or_timestamp="updated_at") }} as {{ dbt.type_timestamp() }}) as updated_at,,
         currency,
         initial_value,
         cast({{ dbt.dateadd(datepart="hour", interval=hr, from_date_or_timestamp="disabled_at") }} as {{ dbt.type_timestamp() }}) as disabled_at,
@@ -76,13 +75,13 @@ select coalesce(max(_daton_batch_runtime) - 2592000000,0) from {{ this }}
         {{daton_batch_runtime()}} as _daton_batch_runtime,
         {{daton_batch_id()}} as _daton_batch_id,
         current_timestamp() as _last_updated,
-        '{{env_var("DBT_CLOUD_RUN_ID", "manual")}}' as _run_id,
+        '{{env_var("DBT_CLOUD_RUN_ID", "manual")}}' as _run_id
         from  {{i}} a
                 {% if is_incremental() %}
                 {# /* -- this filter will only be applied on an incremental run */ #}
                 where {{daton_batch_runtime()}}  >= {{max_loaded}}
                 {% endif %}
-        qualify dense_rank() over (partition by a.id order by {{daton_batch_runtime()}} desc) = 1
+        qualify dense_rank() over (partition by a.id order by {{daton_batch_runtime()}} desc) row_num = 1
 
     {% if not loop.last %} union all {% endif %}
 {% endfor %}
