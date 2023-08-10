e03492337ca984f5631b4df7359e0ab97fdde044
abijithmenon-saras <141811659+abijithmenon-saras@users.noreply.github.com>
2023-08-10 18:46:56 +0530

    Update ShopifyLocations.sql

--- models/Shopify/ShopifyLocations.sql
+++ models/Shopify/ShopifyLocations.sql
@@ -20,7 +20,7 @@ select coalesce(max(_daton_batch_runtime) - 2592000000,0) from {{ this }}
 
 
 {% set table_name_query %}
-{{set_table_name('%shopify%locations')}} and lower(table_name) not like '%googleanalytics%' and lower(table_name) not like 'v1%'
+{{set_table_name('%shopify%locations')}}
 {% endset %}  
 
 
@@ -74,12 +74,12 @@ select coalesce(max(_daton_batch_runtime) - 2592000000,0) from {{ this }}
         localized_country_name,
         localized_province_name,
         address2,
-        phone,
+        cast(phone as numeric) as phone,
         {{daton_user_id()}} as _daton_user_id,
         {{daton_batch_runtime()}} as _daton_batch_runtime,
         {{daton_batch_id()}} as _daton_batch_id,
         current_timestamp() as _last_updated,
-        '{{env_var("DBT_CLOUD_RUN_ID", "manual")}}' as _run_id,
+        '{{env_var("DBT_CLOUD_RUN_ID", "manual")}}' as _run_id
         from  {{i}} a
                 {% if is_incremental() %}
                 {# /* -- this filter will only be applied on an incremental run */ #}
